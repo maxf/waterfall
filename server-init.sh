c680749399dd4e4fb41856fb7993cf0316e07c1b
Max Froumentin <github@lapin-bl.eu>
2017-08-27 12:02:52 +0100

    add package

--- server-init.sh
+++ server-init.sh
@@ -3,9 +3,12 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install apache2 libapache2-mod-php nodejs npm git
+sudo apt-get install apache2 libapache2-mod-php nodejs nodejs-legacy npm git
 
 cd /var/www/html
 sudo git clone https://github.com/maxf/waterfall.git
-git checkout server
+sudo cd waterfall
+sudo git checkout server
 sudo npm install
+sudo mkdir uploads
+sudo chown -R a+rwx .
