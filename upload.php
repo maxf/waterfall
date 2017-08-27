df9efb8a7254e30b80fea55b11e20e71fd8b7fe3
Max Froumentin <github@lapin-bl.eu>
2017-08-27 12:22:04 +0100

    Add exif date in uploaded data file

--- upload.php
+++ upload.php
@@ -1,55 +1,54 @@
 <!DOCTYPE HTML>
 <html>
-<head>
-<meta charset="utf-8">
-<script src="exif.js"></script>
-</head><body>
+  <head>
+    <meta charset="utf-8">
+    <script src="exif.js"></script>
+  </head>
+  <body>
 
-  <?php
+    <?php
 
-  if (count($_POST)) {
+    if (count($_POST)) {
 
-    $img = explode('|', $_POST['img']);
+      $img = explode('|', $_POST['img']);
 
-    for ($i = 0; $i < count($img) - 1; $i++) {
+      $dates = explode('|', $_POST['date']);
 
-      if (strpos($img[$i], 'data:image/jpeg;base64,') === 0) {
+      for ($i = 0; $i < count($img) - 1; $i++) {
+
         $img[$i] = str_replace('data:image/jpeg;base64,', '', $img[$i]);
-        $ext = '.jpg';
-      }
-      if (strpos($img[$i], 'data:image/png;base64,') === 0) {
-        $img[$i] = str_replace('data:image/png;base64,', '', $img[$i]);
-        $ext = '.png';
-      }
+        $img[$i] = str_replace(' ', '+', $img[$i]);
 
-      $img[$i] = str_replace(' ', '+', $img[$i]);
-      $data = base64_decode($img[$i]);
-      $file = 'uploads/img'.date("YmdHis").'_'.$i.$ext;
+        $data = base64_decode($img[$i]);
+        //      $file = 'uploads/img'.date("YmdHis").'_'.$dates[$i].'_'.$i.'.jpg';
 
-      if (file_put_contents($file, $data)) {
-        echo "<p>Image $i was saved as $file.</p>";
-      } else {
-        echo '<p>Image $i could not be saved.</p>';
-      }
-    }
-  }
+        $date = str_replace(' ', '_', $dates[$i]);
+        $file = 'uploads/img_'.$date.'_'.$i.'.jpg';
 
-  ?>
+          if (file_put_contents($file, $data)) {
+            echo "<p>Image $i was saved as $file.</p>";
+          } else {
+            echo '<p>Image $i could not be saved.</p>';
+          }
+        }
+        }
+
+    ?>
 
 
-  <input id="inp_files" type="file" multiple="multiple">
+    <input id="inp_files" type="file" multiple="multiple">
 
-  <form method="post" action="">
-    <input id="inp_img" name="img" type="text" style="width:100%">
-    <br>
-    <input id="exif_date" type="text" value="" style="width:100%">
-    <br>
-    <input id="counter" type="number" value="0"><span> images to process</span>
-    <div id="message">Please select your images</div>
-    <input id="bt_save" type="submit" value="Upload" disabled>
-  </form>
+    <form method="post" action="">
+      <input id="inp_img" name="img" type="text" style="width:100%">
+      <br>
+      <input id="exif_date" name="date" type="text" value="" style="width:100%">
+      <br>
+      <input id="counter" type="number" value="0"><span> images to process</span>
+      <div id="message">Please select your images</div>
+      <input id="bt_save" type="submit" value="Upload" disabled>
+    </form>
 
 
-  <script src="upload.js"></script>
+    <script src="upload.js"></script>
 
-</body></html>
+  </body></html>
