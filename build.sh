c808dd15474d1f0c4ed7dc48d5ef9f075a333216
Max Froumentin <github@lapin-bl.eu>
2017-10-23 23:38:49 +0100

    Use env vars in order to avoid modifying file

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-elm-make *.elm --debug --yes --warn --output=elm.js
+if [ -z $WATERFALL_INSTALL_DIR ]; then
+    echo "You must set WATERFALL_INSTALL_DIR"
+    exit -1
+fi
+
+
+elm-make *.elm $WATERFALL_ELM_DEBUG --yes --warn --output=elm.js
 browserify -o out.js main.js
-rsync -avzr --exclude node_modules --exclude .git --exclude elm-stuff ../waterfall waterfall:/var/www/html
+rsync -avzr --exclude node_modules --exclude .git --exclude elm-stuff ../waterfall $WATERFALL_INSTALL_DIR
