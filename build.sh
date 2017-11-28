e1954224514fd9343f79599b665ce88862ff6258
Max Froumentin <github@lapin-bl.eu>
2017-11-28 23:51:53 +0000

    Read target from arguments

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-elm-make *.elm $WATERFALL_ELM_DEBUG --yes --warn --debug --output=public/elm.js
+elm-make *.elm $WATERFALL_ELM_DEBUG --yes --warn --output=public/elm.js
 browserify -o public/out.js main.js
 
-if [ -z $WATERFALL_INSTALL_DIR ]; then
-    sync -avzr --exclude node_modules --exclude .\* --exclude elm-stuff ../waterfall $WATERFALL_INSTALL_DIR
+if [ -z "$1" ]; then
+    echo Not copying
 else
-    echo "WATERFALL_INSTALL_DIR not set."
+    rsync -avzr --exclude node_modules --exclude .\* --exclude elm-stuff ../waterfall $1
 fi
